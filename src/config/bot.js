ae2dbd9947925bc13021b3e8b3e5b0960e5bd6ac
aliff7bd <ldixwh123@gmail.com>
2026-05-10 00:08:52 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "sub to @L8cpvp",
         // Activity type number (0 = Playing).
         type: 0, 
       },
